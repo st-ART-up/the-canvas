2a5f6876fc89826d9c5891818ccdd018e3a75088
Kat Sauma <katsauma@gmail.com>
2021-04-28 10:31:24 -0700

    Kat dev (#16)
    * add dev branch (#1)
    
    * canvas color scheme, palette, and hover colors implemented in design for terminal.
    
    * input bar and button send text to canvas
    
    * button colors match, input bar sizing fixed, menubar button added and sizes changed
    
    * api-utils file with function to upload png from a saved image
    
    * updates on upload png functionality
    
    Co-authored-by: Nicole Martin <71411431+nicole-m-martin@users.noreply.github.com>

--- utils/menu-button-utils.js
+++ utils/menu-button-utils.js
@@ -1,11 +1,33 @@
 const blessed = require('blessed');
 const { screen, canvas } = require('../interface/startup-screen');
+const imgur = require('./imgur-utils');
+const saveToDb = require('./api-utils');
+
 
 function clearCanvas(paint) {
     paint.destroy();
     screen.render();
 }
 
+async function uploadPng() {
+// get file path from save function
+const drawingUrl = await imgur();
+console.log(drawingUrl, 'url');
+// append box with form
+const png = {
+    drawingUrl,
+    artist: 'Basquiat', 
+    title: 'bacon and eggs, make it vegan', 
+    caption: 'not much to eat, but it looks ok'
+}
+// form sent to db /POST 
+saveToDb(png);
+
+// console.log('success is ours to keep');
+}
+
+
 module.exports = {
-    clearCanvas
+    clearCanvas,
+    uploadPng
 }
