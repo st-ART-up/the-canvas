b7cf7588d0ee2d7a6ebcdd5fdd7673d36cdf119b
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-04-28 11:09:02 -0700

    made picture save endpoint work.

--- utils/menu-button-utils.js
+++ utils/menu-button-utils.js
@@ -3,31 +3,30 @@ const { screen, canvas } = require('../interface/startup-screen');
 const imgur = require('./imgur-utils');
 const saveToDb = require('./api-utils');
 
-
 function clearCanvas(paint) {
-    paint.destroy();
-    screen.render();
+  paint.destroy();
+  screen.render();
 }
 
 async function uploadPng() {
-// get file path from save function
-const drawingUrl = await imgur();
-console.log(drawingUrl, 'url');
-// append box with form
-const png = {
-    drawingUrl,
-    artist: 'Basquiat', 
-    title: 'bacon and eggs, make it vegan', 
-    caption: 'not much to eat, but it looks ok'
-}
-// form sent to db /POST 
-saveToDb(png);
+  // get file path from save function
+  console.log('start');
+  const drawingUrl = await imgur();
+  console.log(drawingUrl, 'url');
+  // append box with form
+  const png = {
+    drawingUrl: drawingUrl,
+    artist: 'new artist!',
+    title: 'a new work',
+    caption: 'not as great as the old work',
+  };
+  // form sent to db /POST
+  saveToDb(png);
 
-// console.log('success is ours to keep');
+  // console.log('success is ours to keep');
 }
-
 
 module.exports = {
-    clearCanvas,
-    uploadPng
-}
+  clearCanvas,
+  uploadPng,
+};
