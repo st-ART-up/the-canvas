5ff7ec38049c967cc75a59b1405b8098cb310d1a
Erich Sauer <73718130+erichsauer@users.noreply.github.com>
2021-04-28 17:45:25 -0700

    Erich (#20)
    * add dev branch (#1)
    
    * fixes oauth routing
    
    Co-authored-by: Nicole Martin <71411431+nicole-m-martin@users.noreply.github.com>

--- utils/api-utils.js
+++ utils/api-utils.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 const fs = require('fs').promises;
 
-module.exports = async (form) => {
+const URL = 'https://st-art-up.herokuapp.com/api/v1';
+// const URL = 'http://localhost:7890/api/v1'
 
-    return axios({
-        url: 'https://st-art-up.herokuapp.com/api/v1/drawings',
-        method: 'POST',
-        headers: {
-        },
-        data: form
-    })
-        .then((data) => console.log(data));
+module.exports = async (form) => {
+  return axios({
+    url: `${URL}/drawings`,
+    method: 'POST',
+    headers: {},
+    data: form,
+  }).then((data) => console.log(data));
 };
