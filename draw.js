4cd25d1501067f0cf530b0b8ce390514fe753f43
Kat Sauma <katsauma@gmail.com>
2021-04-29 11:58:14 -0700

    Hot mess (#21)
    * add dev branch (#1)
    
    * canvas color scheme, palette, and hover colors implemented in design for terminal.
    
    * input bar and button send text to canvas
    
    * button colors match, input bar sizing fixed, menubar button added and sizes changed
    
    * api-utils file with function to upload png from a saved image
    
    * updates on upload png functionality
    
    * notes has colors list
    
    * black switch to onyx. gallery.js add. bin hash add.
    
    Co-authored-by: Nicole Martin <71411431+nicole-m-martin@users.noreply.github.com>

--- draw.js
+++ draw.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 module.exports = () => {
   return new Promise(resolve => {
     const blessed = require('blessed');
@@ -51,6 +52,11 @@ module.exports = () => {
       });
       // screen.render();
     });
+
+    // screen.key(['u'], function (ch, key){
+    //   undoButton.deleteLine(i);
+    //   screen.render();
+    // });
 
     uploadButton.on('click', function (mouse) {
       uploadPng();
