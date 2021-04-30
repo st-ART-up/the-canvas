8acd7dbb08612b95c754c1b0676bdf88c9cd9a88
Erich Sauer <erich.sauer@gmail.com>
2021-04-29 19:14:40 -0700

    refactors to follow canvas flow

--- gallery.js
+++ gallery.js
@@ -2,7 +2,6 @@
 
 const inquirer = require('inquirer');
 const auth = require('./utils/auth-utils');
-const draw = require('./draw');
 const {
   galleryPrompt,
   welcomePrompt,
@@ -18,67 +17,69 @@ const {
   logUserIn,
 } = require('./utils/gallery-utils');
 
-const stARTupSkeleton = (newPrompt) => {
-  return inquirer
-    .prompt({
-      type: newPrompt.type,
-      message: newPrompt.message,
-      name: newPrompt.name,
-      choices: newPrompt.choices,
-    })
-    .then((response) => {
-      switch (response.option) {
-        case 'log in to edit my drawings':
-          logInSkeleton();
-          break;
-        case 'show me the art!':
-          stARTupSkeleton(galleryPrompt);
-          break;
-        case 'Exit':
-          console.log('See you later!');
-          break;
-        case 'View your drawings':
-          getUserDrawings().then(() => stARTupSkeleton(galleryPrompt));
-          break;
-        case 'View all drawings':
-          getAllImages().then(() => stARTupSkeleton(galleryPrompt));
-          break;
-        case 'View random drawings':
-          getRandomImage().then(() => stARTupSkeleton(galleryPrompt));
-          break;
-        case 'view my drawings':
-          getUserDrawings(userToken).then(() => stARTupSkeleton(userPrompt));
-          //   console.log('getall', userToken);
-          break;
-        case 'delete a drawing by its ID':
-          deleteSkeleton(userToken).then(() => {
-            console.log('DELETED FOREVER'), stARTupSkeleton(userPrompt);
-          });
-          break;
-        case 'Go back to main menu':
-          stARTupSkeleton(welcomePrompt);
+module.exports = () => {
+  const stARTupSkeleton = (newPrompt) => {
+    return inquirer
+      .prompt({
+        type: newPrompt.type,
+        message: newPrompt.message,
+        name: newPrompt.name,
+        choices: newPrompt.choices,
+      })
+      .then((response) => {
+        switch (response.option) {
+          case 'log in to edit my drawings':
+            logInSkeleton();
+            break;
+          case 'show me the art!':
+            stARTupSkeleton(galleryPrompt);
+            break;
+          case 'Exit':
+            console.log('See you later!');
+            break;
+          case 'View your drawings':
+            getUserDrawings().then(() => stARTupSkeleton(galleryPrompt));
+            break;
+          case 'View all drawings':
+            getAllImages().then(() => stARTupSkeleton(galleryPrompt));
+            break;
+          case 'View random drawings':
+            getRandomImage().then(() => stARTupSkeleton(galleryPrompt));
+            break;
+          case 'view my drawings':
+            getUserDrawings(userToken).then(() => stARTupSkeleton(userPrompt));
+            //   console.log('getall', userToken);
+            break;
+          case 'delete a drawing by its ID':
+            deleteSkeleton(userToken).then(() => {
+              console.log('DELETED FOREVER'), stARTupSkeleton(userPrompt);
+            });
+            break;
+          case 'Go back to main menu':
+            stARTupSkeleton(welcomePrompt);
+        }
+      });
+  };
+  let userToken;
+  const logInSkeleton = () => {
+    inquirer.prompt(loginPrompt).then((response) => {
+      if (response.githubAuth === true) {
+        auth().then((token) => {
+          // deleteSkeleton(token),
+          logUserIn(token), stARTupSkeleton(userPrompt), (userToken = token);
+        });
+      } else {
+        console.log('Please create a github account to log in to stARTup');
       }
     });
-};
-let userToken;
-const logInSkeleton = () => {
-  inquirer.prompt(loginPrompt).then((response) => {
-    if (response.githubAuth === true) {
-      auth().then((token) => {
-        // deleteSkeleton(token),
-        logUserIn(token), stARTupSkeleton(userPrompt), (userToken = token);
-      });
-    } else {
-      console.log('Please create a github account to log in to stARTup');
-    }
-  });
-};
+  };
 
-const deleteSkeleton = (token) => {
-  inquirer.prompt(deletePrompt).then((response) => {
-    // deleteADrawing(response.deleteIt, token);
-    console.log('userToken:', token);
-  });
-};
+  const deleteSkeleton = (token) => {
+    inquirer.prompt(deletePrompt).then((response) => {
+      // deleteADrawing(response.deleteIt, token);
+      console.log('userToken:', token);
+    });
+  };
 
-stARTupSkeleton(welcomePrompt);
+  stARTupSkeleton(welcomePrompt);
+};
---
+++ look.js
@@ -0,0 +1,12 @@
+const look = require('./gallery');
+const { titleScreen } = require('./title-screen');
+
+function start(callback) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve();
+    }, 5000);
+  });
+}
+
+start(titleScreen).then(look);
